7fabc50a58ea0b9c43a0f0a378a6f28ebe6e692b
Himu-Vai <71009349+Himu-Vai@users.noreply.github.com>
2020-10-01 10:43:44 +0600

    Add files via upload

---
+++ Operator/src/operator/Operator.java
@@ -0,0 +1,17 @@
+
+package operator;
+
+
+
+public class Operator {
+
+    
+    public static void main(String[] args) {
+        int a=5,b;
+        b=a++;
+        System.out.println("value of b:"+b+" The value of a:"+a);
+        b=++a;
+        System.out.println("value of b:"+b+" The value of a:"+a);
+    }
+    
+}
